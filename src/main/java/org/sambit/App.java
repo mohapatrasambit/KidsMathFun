96b210e57b8144973c89384fc49663de73544966
Sambit Mohapatra <mohapatra.sambit@live.com>
2023-02-28 20:48:25 -0800

    Moved name and time entry to header of page (#3)

--- src/main/java/org/sambit/App.java
+++ src/main/java/org/sambit/App.java
@@ -32,7 +32,7 @@ public class App
             case 1:
             default:
                 equation = new Additions();
-                equations = equation.generateEquations(42, 20);
+                equations = equation.generateEquations(57, 15);
                 break;
             case 2:
                 equation = new RandomStandardNumberTable();
